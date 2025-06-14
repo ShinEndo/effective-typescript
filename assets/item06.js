60f3f0585135f61e8152c5855d5c724e3573f3d6
遠藤慎 <end3mame@gmail.com>
2025-06-14 23:08:04 +0900

    feat: エディターを使って型システムを調査、探究する

---
+++ assets/item06.js
@@ -0,0 +1,23 @@
+"use strict";
+function getElement(elOrId) {
+    if (elOrId === null) {
+        return document.body;
+    }
+    else if (typeof elOrId === 'object') {
+        return elOrId;
+    }
+    const el = document.getElementById(elOrId);
+    if (!el) {
+        throw new Error(`No such element ${elOrId}`);
+    }
+    return el;
+}
+let i = 0;
+for (let x = 0; x < 10; x++) {
+    console.log(x);
+    {
+        let i = 122;
+        console.log(i);
+    }
+}
+console.log(i);
