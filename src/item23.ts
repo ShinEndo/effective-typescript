951de340fc233a367a8dde3323b00bad94ca0f57
遠藤慎 <end3mame@gmail.com>
2025-06-15 15:03:50 +0900

    feat: エイリアスを作成したら一貫してそれを使う

---
+++ src/item23.ts
@@ -0,0 +1,26 @@
+interface Coordinate {
+	x: number;
+	y: number;
+}
+
+interface BoundingBox {
+	x: [number,number];
+	y: [number,number];
+}
+
+interface Polygon {
+	exterior: Coordinate[];
+	hotels: Coordinate[][];
+	bbox?: BoundingBox;
+}
+
+function isPointInPolygon(polygon: Polygon, pt: Coordinate) {
+	const { bbox } = polygon;
+	if(bbox) {
+		const { x,y } = bbox;
+		if(pt.x < x[0] || pt.x > x[1] ||
+		   pt.y < y[0] || pt.y > y[1]) {
+				return false;
+		}
+	}
+}
